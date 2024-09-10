f9b58ec7b282b1bffe0dd37ba9d2d91b0f66e10c
stdonnelly <80422933+stdonnelly@users.noreply.github.com>
2024-09-10 15:43:42 -0400

    Start adding job application repository

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use repository::job_application_repository::get_job_applications;
+
+mod repository {
+    pub mod job_application_repository;
+}
+
 fn main() {
-    println!("Hello, world!");
+    todo!();
 }
