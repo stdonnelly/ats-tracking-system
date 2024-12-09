fc536c677a3e050911a4a150869b1d7b81955856
stdonnelly <80422933+stdonnelly@users.noreply.github.com>
2024-12-08 20:14:41 -0500

    Move get_conn() to repository crate

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,4 @@
-use std::env;
-
 use dotenv::dotenv;
-use mysql::{OptsBuilder, Pool, PooledConn};
 
 mod command_line;
 mod shell_option;
@@ -9,32 +6,7 @@ mod shell_option;
 fn main() {
     // Objects that should be owned by the main function
     dotenv().unwrap();
-    let mut conn = get_conn().unwrap();
+    let mut conn = repository::get_conn().unwrap();
 
     command_line::main_loop(&mut conn).unwrap();
-}
-
-/// Boilerplate for getting connection information from environment
-fn get_conn() -> Result<PooledConn, mysql::Error> {
-    let mut sql_opts_builder = OptsBuilder::new();
-    if let Ok(host_name) = env::var("DB_HOST") {
-        sql_opts_builder = sql_opts_builder.ip_or_hostname(Some(host_name));
-    }
-    if let Ok(port_str) = env::var("DB_PORT") {
-        if let Ok(port_int) = port_str.parse::<u16>() {
-            sql_opts_builder = sql_opts_builder.tcp_port(port_int);
-        }
-    }
-    if let Ok(user) = env::var("DB_USER") {
-        sql_opts_builder = sql_opts_builder.user(Some(user));
-    }
-    if let Ok(pass) = env::var("DB_PASSWORD") {
-        sql_opts_builder = sql_opts_builder.pass(Some(pass));
-    }
-    if let Ok(db_name) = env::var("DB_DATABASE") {
-        sql_opts_builder = sql_opts_builder.db_name(Some(db_name));
-    }
-
-    let pool = Pool::new(sql_opts_builder)?;
-    pool.get_conn()
 }
--- repository/src/lib.rs
+++ repository/src/lib.rs
@@ -1,2 +1,33 @@
+use std::env;
+
+use mysql::{OptsBuilder, Pool, PooledConn};
+
 pub mod job_application_model;
 pub mod job_application_repository;
+
+/// Get a connection object to be used by the rest of this crate
+///
+/// This exists so that the main function can own the connection object instead of creating a new one for every call
+pub fn get_conn() -> Result<PooledConn, mysql::Error> {
+    let mut sql_opts_builder = OptsBuilder::new();
+    if let Ok(host_name) = env::var("DB_HOST") {
+        sql_opts_builder = sql_opts_builder.ip_or_hostname(Some(host_name));
+    }
+    if let Ok(port_str) = env::var("DB_PORT") {
+        if let Ok(port_int) = port_str.parse::<u16>() {
+            sql_opts_builder = sql_opts_builder.tcp_port(port_int);
+        }
+    }
+    if let Ok(user) = env::var("DB_USER") {
+        sql_opts_builder = sql_opts_builder.user(Some(user));
+    }
+    if let Ok(pass) = env::var("DB_PASSWORD") {
+        sql_opts_builder = sql_opts_builder.pass(Some(pass));
+    }
+    if let Ok(db_name) = env::var("DB_DATABASE") {
+        sql_opts_builder = sql_opts_builder.db_name(Some(db_name));
+    }
+
+    let pool = Pool::new(sql_opts_builder)?;
+    pool.get_conn()
+}
